d1d9360359962f8c22b2dd41da11526900e17467
Viraat Das <viraat.laldas@gmail.com>
2024-07-10 09:37:53 -0700

    update pyproject.toml - prolly need to change project names

--- setup.py
+++ setup.py
@@ -2,14 +2,27 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycrate',
-    version='0.1',
+    version='0.1.0',
+    author='Viraat Das',
+    author_email='viraat.laldas@gmail.com',
+    description='Cargo for Python - a hassle-free package manager for Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/viraatdas/pycrate',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'click',
+        'toml',
     ],
     entry_points='''
         [console_scripts]
         pycrate=pycrate.cli:cli
     ''',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
